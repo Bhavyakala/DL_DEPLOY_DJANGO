ea2faa1e6b464387dc18a69f238be66dffb01fed
Bhavya Kala <bhavya.kala10@gmail.com>
2020-05-24 19:40:12 +0530

    minor changes

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -20,9 +20,9 @@ app_name = "main"
 
 urlpatterns = [
     path('',views.homepage, name='homepage'),
-    path('register', views.register, name='register'),
-    path('logout', views.logout_request, name='logout'),
-    path('login', views.login_request, name='login'),
+    path('register/', views.register, name='register'),
+    path('logout/', views.logout_request, name='logout'),
+    path('login/', views.login_request, name='login'),
     path('<single_slug>', views.single_slug, name='single_slug'),
 ]
  
